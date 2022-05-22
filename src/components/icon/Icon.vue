c47f83a7c6e03f07f2d458f50ae09886e7ad83e5
chibek <sergibelemarketing@gmail.com>
2022-05-23 00:00:40 +0200

    Add component icon for render icons svg

---
+++ src/components/icon/Icon.vue
@@ -0,0 +1,44 @@
+<template>
+    <component :is="currentIcon" :class="iconClass" />
+</template>
+<script>
+import { computed, defineComponent, defineAsyncComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        current: {
+            type: String,
+            validator: (value) =>
+                ['stroke', 'fill', 'stroke-fill', 'none'].includes(value),
+            default: 'stroke',
+        },
+    },
+    setup(props) {
+        const currentIcon = computed(() => {
+            try {
+                return defineAsyncComponent(() =>
+                    import(`../../assets/icons/${props.name}.svg?component`),
+                );
+            } catch (e) {
+                console.log('Not icon found');
+            }
+        });
+        const iconClass = computed(() => {
+            if (props.current === 'stroke-fill')
+                return 'stroke-current fill-current';
+            if (props.current === 'fill') return 'stroke-current';
+            if (props.current === 'stroke') return 'fill-current';
+            return '';
+        });
+
+        return {
+            currentIcon,
+            iconClass,
+        };
+    },
+});
+</script>
