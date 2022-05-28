9959f6a0fe0f4e967056434fe9b9bb2661f8a57f
chibek <sergibelemarketing@gmail.com>
2022-05-29 00:42:51 +0200

    Add New api endpoints

--- src/api/context-url/index.js
+++ src/api/context-url/index.js
@@ -29,3 +29,54 @@ export const CONTEXT_URL_BY_ID = gql`
     }
   }
 `;
+
+export const CONTEXT_URL_CREATE = gql`
+  mutation create($input: CreateUrlInput!) {
+    create(createUrl: $input) {
+      id
+      name
+      shortUrl
+      longUrl
+      customUrl
+      clicks
+      created_at
+      updated_at
+    }
+  }
+`;
+
+export const CONTEXT_URL_UPDATE = gql`
+  mutation update($input: UpdateUrlInput!) {
+    update(updateUrl: $input) {
+      id
+      name
+      shortUrl
+      longUrl
+      customUrl
+      clicks
+      created_at
+      updated_at
+    }
+  }
+`;
+
+export const CONTEXT_URL_UPDATE_CLICK = gql`
+  mutation create($input: String!) {
+    updateClick(id: $input) {
+      id
+      name
+      shortUrl
+      longUrl
+      customUrl
+      clicks
+      created_at
+      updated_at
+    }
+  }
+`;
+
+export const CONTEXT_URL_DELETE = gql`
+  mutation ($input: String!) {
+    delete(id: $input)
+  }
+`;
