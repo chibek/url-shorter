6651b3aeccfcc92ebdaabfcd46e5d5ba1c241515
chibek <sergibelemarketing@gmail.com>
2022-05-28 22:03:59 +0200

    Configuration and libraries, css

--- src/pages/Link.vue
+++ src/pages/Link.vue
@@ -1,15 +1,35 @@
 <template>
-  <section class="flex flex-col space-y-2 p-6" v-if="!loading">
+  <section class="flex flex-col space-y-2 p-6 select-none" v-if="!urlLoading">
     <div class="flex items-center justify-between">
       <h2 class="text-xl font-bold">{{ url.name }}</h2>
-      <button class="">Edit</button>
+      <button
+        class="trans-all flex cursor-pointer space-x-2 rounded-lg p-2 hover:bg-gray-200 hover:text-orange-400 border shadow-lg"
+      >
+        <Icon name="pencil" class="h-5 w-5" />
+        <span>Edit</span>
+      </button>
     </div>
     <span>{{ url.created_at }}</span>
-    <div class="boder-blue-200 rounded-lg border p-2">
-      {{ url.shortUrl }}
+    <LinkRedirect @copy="copy($event)" :url="url" />
+    <div class="flex items-center space-x-1 text-sm font-bold">
+      <Icon name="switch" class="h-4 w-4" />
+      <span>Destination:</span>
     </div>
-    <span class="text-sm font-bold"> Destination: </span>
-    <a :href="url.longUrl">{{ url.longUrl }}</a>
+    <div>
+      <a
+        class="ml-5"
+        target="_blank"
+        :href="url.longUrl"
+        >{{ url.longUrl }}</a
+      >
+    </div>
+    <div class="flex flex-col">
+      <div class="flex items-end space-x-1">
+        <span class="text-2xl font-bold">{{ url.clicks }}</span>
+        <Icon name="chart" />
+      </div>
+      <span class="text-sm">Total clicks</span>
+    </div>
   </section>
   <section class="flex h-full w-full items-center justify-center" v-else>
     Loading...
@@ -17,26 +37,32 @@
 </template>
 
 <script setup>
-import { useQuery } from "@vue/apollo-composable";
+import { useLazyQuery } from "@vue/apollo-composable";
 import { CONTEXT_URL_BY_ID } from "../api/context-url";
-import { computed, reactive, ref, watchEffect } from "vue";
+import { computed, reactive, watchEffect } from "vue";
+import Icon from "../components/icon/Icon.vue";
+import LinkRedirect from "../modules/link/LinkRedirect.vue";
+import useClipboard from "../composables/useClipboard.js";
 
-const enabled = ref(false);
 const props = defineProps(["id"]);
-const variables = reactive({
+const { copy } = useClipboard();
+const urlVariables = reactive({
   id: null,
 });
-const { result, loading } = useQuery(CONTEXT_URL_BY_ID, variables, () => ({
-  enabled: enabled.value,
-}));
+
+const {
+  result: urlResult,
+  loading: urlLoading,
+  load: urlLoad,
+} = useLazyQuery(CONTEXT_URL_BY_ID, urlVariables);
 
 watchEffect(async () => {
   if (!props.id) {
     return;
   }
-  variables.id = props.id;
-  enabled.value = true;
+  urlVariables.id = props.id;
+  urlLoad();
 });
 
-const url = computed(() => result.value?.findOne ?? {});
+const url = computed(() => urlResult.value?.findOne ?? {});
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import svgLoader from "vite-svg-loader";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default ({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd()));
+  return defineConfig({
+    server: {
+      proxy: {
+        "/graphql": {
+          target: "http://localhost:3004/graphql",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+    plugins: [
+      vue(),
+      svgLoader({
+        svgoConfig: {
+          multipass: true,
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  });
+};
