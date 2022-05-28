fa05efb275552bcf11b8958bd794f5d519317d74
chibek <sergibelemarketing@gmail.com>
2022-05-28 22:02:16 +0200

    Create component redirect

---
+++ src/modules/link/LinkRedirect.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="boder-blue-200 flex justify-between rounded-lg border p-2">
+    <a target="_blank" :href="url.longUrl" class="flex items-center space-x-2">
+      <Icon name="link" />
+      <span>{{ shortUrl }}</span>
+    </a>
+
+    <div
+      class="trans-all flex cursor-pointer space-x-2 rounded-lg p-2 hover:bg-gray-200 hover:text-orange-400"
+      @click="emit('copy', shortUrl)"
+    >
+      <Icon name="clipboard" />
+      <span>Copy</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import Icon from "../../components/icon/Icon.vue";
+
+const props = defineProps(["url"]);
+const emit = defineEmits(["copy"]);
+const shortUrl = computed(
+  () => `${import.meta.env.VITE_BACK_API}/link/${props.url.shortUrl}`
+);
+</script>
